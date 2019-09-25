20055402723afc877b8e9ad5aecc27edfe719bcf
IRMAN NOVRYANSAH <55683592+IrmanN48@users.noreply.github.com>
2019-09-25 22:19:18 +0700

    Add files via upload

---
+++ src/latihan1/biodata/Latihan1Biodata.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1.biodata;
+
+/**
+/*BIODATA MAHASISWA*\
+ *
+ * @author Aero
+ */
+public class Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("BIODATA ROCSTAR" + "\n" + "------------------");
+        System.out.println("NAMA\t\t: Irman Novryansah");
+        System.out.println("ALAMAT\t\t: Jln.Holis");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t : 30 November 1998");
+        System.out.println("--------------------");
+        System.out.println("HOBI\t\t: Photografer,Hunting");
+        System.out.println("MAKES\t\t: Soto Ayam");
+        System.out.println("MIKES\t\t: Anggur Merah");
+        System.out.println("IDOLA\t\t: Jessica Mila"); 
+      
+        
+    }
+    
+}
